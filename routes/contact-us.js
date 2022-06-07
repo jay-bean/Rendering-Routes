77b9210554c89e026acc8f00003358a507b363bb
Seigo <exequiel.hashimoto@outlook.com>
2022-06-07 14:09:58 -0700

    starting on contact-us page for routes from the home page (index)

---
+++ routes/contact-us.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {csrfProtection, asyncHandler} = require('./utils');
+const { requireAuth } = require('../auth');
+
+router.get('/', function(req, res, next) {
+    res.render('contact-us', { title: 'Meet the Development Team'});
+});
