3f9157f5a2a0f48d6079b3e5312aaf31fb2f6384
Seigo <exequiel.hashimoto@outlook.com>
2022-06-07 16:23:18 -0700

    cycling through routes on the home page, and more button functionality for user navigation

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 const {csrfProtection, asyncHandler} = require('./utils');
 const { requireAuth } = require('../auth');
+const db = require('../db/models');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rendering Routes' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const displayRoutes = await db.Routes.findAll({ limit: 5 });
+  const cycleRoutes = await db.Routes.findAll({ limit: 3});  
+  res.render('index', { title: 'Rendering Routes', cycleRoutes });
+}));
 
 router.get('/404', function(req, res, next) {
   res.render('404');
