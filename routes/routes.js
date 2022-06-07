ce84e0cc63d120278f131fb0de871ca1fbbb15be
Jay Hutts <janieehutts@gmail.com>
2022-06-07 10:55:07 -0600

    progress on routes.js and creating route form

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,65 @@
 const express = require('express');
-
+const csrf = require('csurf');
 const router = express.Router();
 
 const db = require('../db/models');
-const { asyncHandler } = require('./utils');
+const { asyncHandler, csrfProtection } = require('./utils');
 
 router.get('/', asyncHandler(async (req, res) => {
-  const routes = await db.Route.findAll();
+  const routes = await db.Route.findAll({ order: [['name', 'ASC']] });
   res.render('routes', {
     title: 'Routes',
     routes
   })
 }));
+
+router.get('route/add', csrfProtection, (req, res) => {
+  const route = db.Route.build();
+  res.render('route-add', {
+    title: 'Add Route',
+    route,
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.post('route/add', csrfProtection, asyncHandler(async (req, res) => {
+  const {
+    name,
+    description,
+    image,
+    difficulty,
+    height,
+    protection,
+    type
+  } = req.body;
+
+  const route = db.Route.build({
+    name,
+    description,
+    image,
+    difficulty,
+    height,
+    protection,
+    type
+  });
+
+  try {
+    await route.save();
+    res.redirect(`/${route.id}`);
+  }
+  catch (err) {
+    res.render('route-add', {
+      title: 'Add Route',
+      route,
+      error: err,
+      csrfToken: req.csrfToken()
+    });
+  }
+}));
+
+// router.get('/:id', asyncHandler(async (req, res => {
+//   const route = await db.Route.findByP
+// }));
 
 
 module.exports = router;
