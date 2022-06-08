cfce2e64d6e111782717408a48664c5510fba9bd
Jay Hutts <janieehutts@gmail.com>
2022-06-08 13:28:32 -0600

    finished route edit

--- public/javascripts/routeEdit.js
+++ public/javascripts/routeEdit.js
@@ -1,4 +1,4 @@
-const editButton = document.getElementById('route-edit-button');
+const editButton = document.querySelector('#route-edit-button');
 
 if (editButton) {
   const splitURL = document.URL.split('/');
@@ -10,6 +10,7 @@ if (editButton) {
     if (postInfo.classList.contains('hidden')) {
       postInfo.classList.remove('hidden');
       formInfo.classList.add('hidden');
+      editButton.innerText = 'Edit';
     }
     else {
       postInfo.classList.add('hidden');
@@ -17,17 +18,7 @@ if (editButton) {
       editButton.innerHTML = 'Cancel';
     }
   });
-  const cancelButton = document.querySelector(`#route-edit-cancel-${routeId}`);
-  cancelButton.addEventListener('click', (e) => {
-    if (postInfo.classList.contains('hidden')) {
-      postInfo.classList.remove('hidden');
-      formInfo.classList.add('hidden');
-    }
-    else {
-      postInfo.classList.add('hidden');
-      formInfo.classList.remove('hidden');
-    }
-  });
+
   const submitButton = document.querySelector(`#route-edit-submit-${routeId}`);
   submitButton.addEventListener('click', async (e) => {
     e.preventDefault();
@@ -38,6 +29,16 @@ if (editButton) {
     const type = document.querySelector(`#route-${routeId}-edit-type`).value;
     const protection = document.querySelector(`#route-${routeId}-edit-protection`).value;
     const description = document.querySelector(`#route-${routeId}-edit-description`).value;
+    const cragDropDown = document.getElementById(`route-${routeId}-edit-crag`);
+    const crag = cragDropDown.options[cragDropDown.selectedIndex].value;
+    const cragArr = crag.split('-');
+    console.log(cragArr);
+
+    const cragId = cragArr[0];
+    const cragName = cragArr[1];
+    console.log('cragId', cragId);
+    console.log('cragN', cragName);
+
 
     const res = await fetch(`/routes/${routeId}`, {
       method: 'PATCH',
@@ -48,32 +49,41 @@ if (editButton) {
           height,
           type,
           protection,
-          description
+          description,
+          cragName,
+          cragId
       })
     });
+
+
+    const data = await res.json();
+    const errorContainer = document.querySelector('#route-error-container');
+    if (data.message === 'Success!') {
+      const nameChange = document.querySelector(`#route-${routeId}-name`);
+      const difficultyChange = document.querySelector(`#route-${routeId}-difficulty`);
+      const heightChange = document.querySelector(`#route-${routeId}-height`);
+      const typeChange = document.querySelector(`#route-${routeId}-type`);
+      const protectionChange = document.querySelector(`#route-${routeId}-protection`);
+      const descriptionChange = document.querySelector(`#route-${routeId}-description`);
+      const cragChange = document.querySelector(`#route-${routeId}-crag`);
+      console.log('cragChange', cragChange)
+      console.log('cragName2', cragName)
+      nameChange.innerHTML = data.route.name;
+      difficultyChange.innerHTML = data.route.difficulty;
+      heightChange.innerHTML = data.route.height;
+      typeChange.innerHTML = data.route.type;
+      protectionChange.innerHTML = data.route.protection;
+      descriptionChange.innerHTML = data.route.description;
+      cragChange.innerHTML = data.cragName;
+
+
+      postInfo.classList.remove('hidden');
+      formInfo.classList.add('hidden');
+    }
+    else {
+      data.errors.forEach(error => {
+        errorContainer.innerHTML += `<li>${error}</li>`;
+      })
+    }
   });
-
-  const data = await res.json();
-  if (data.message === 'Success!') {
-    const nameChange = document.querySelector(`#route-${routeId}-name`).value;
-    const difficultyChange = document.querySelector(`#route-${routeId}-difficulty`).value;
-    const heightChange = document.querySelector(`#route-${routeId}-height`).value;
-    const typeChange = document.querySelector(`#route-${routeId}-type`).value;
-    const protectionChange = document.querySelector(`#route-${routeId}-protection`).value;
-    const descriptionChange = document.querySelector(`#route-${routeId}-description`).value;
-
-    nameChange.innerHTML = data.route.name;
-    difficultyChange.innerHTML = data.route.difficulty;
-    heightChange.innerHTML = data.route.height;
-    typeChange.innerHTML = data.route.type;
-    protectionChange.innerHTML = data.route.protection;
-    descriptionChange.innerHTML = data.route.description;
-
-
-    postInfo.classList.remove('hidden');
-    formInfo.classList.add('hidden');
-  }
-  else {
-
-  }
 }
