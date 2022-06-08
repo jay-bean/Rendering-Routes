6a0699bbf816b265d9985bdf8a34e60a45d1c094
Jay Hutts <janieehutts@gmail.com>
2022-06-07 22:12:14 -0600

    start of edit route

--- public/javascripts/routeEdit.js
+++ public/javascripts/routeEdit.js
@@ -1,7 +1,53 @@
-const editButton = document.getElementById('route-edit-btn');
-const form = document.getElementById('route-edit-form');
+const editButton = document.getElementById('route-edit-button');
 
-editButton.addEventListener('click', (e) => {
-  const h1 = document.body.getElementsByTagName('h1');
-  h1.color = red;
-})
+if (editButton) {
+  const splitURL = document.URL.split('/');
+  const routeId = splitURL[splitURL.length - 1];
+  const postInfo = document.querySelector(`#route-post-${routeId}`);
+  const formInfo = document.querySelector(`#route-edit-form-${routeId}`);
+
+  editButton.addEventListener('click', (e) => {
+    if (postInfo.classList.contains('hidden')) {
+      postInfo.classList.remove('hidden');
+      formInfo.classList.add('hidden');
+    }
+    else {
+      postInfo.classList.add('hidden');
+      formInfo.classList.remove('hidden');
+    }
+  });
+  const cancelButton = document.querySelector(`#route-edit-cancel-${routeId}`);
+  cancelButton.addEventListener('click', (e) => {
+    if (postInfo.classList.contains('hidden')) {
+      postInfo.classList.remove('hidden');
+      formInfo.classList.add('hidden');
+    }
+    else {
+      postInfo.classList.add('hidden');
+      formInfo.classList.remove('hidden');
+    }
+  });
+  const submitButton = document.querySelector(`#route-edit-submit-${routeId}`);
+  submitButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector(`#route-${routeId}-edit-name`).value;
+    const difficulty = document.querySelector(`#route-${routeId}-edit-difficulty`).value;
+    const height = document.querySelector(`#route-${routeId}-edit-height`).value;
+    const type = document.querySelector(`#route-${routeId}-edit-type`).value;
+    const protection = document.querySelector(`#route-${routeId}-edit-protection`).value;
+    const description = document.querySelector(`#route-${routeId}-edit-description`).value;
+
+    const res = await fetch(`/routes/${routeId}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          name,
+          difficulty,
+          height,
+          type,
+          protection,
+          description
+    })
+  });
+}
