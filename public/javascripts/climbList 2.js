8424f9df662545858206dfc251e2f3b497e722cd
Lynn Luong <lynn.h.luong@gmail.com>
2022-06-09 22:09:18 -0400

    removed duplicate climb list script

--- public/javascripts/climbList 2.js
+++
@@ -1,8 +0,0 @@
-export const saveAddToClimbList = () => {
-const input = document.querySelector('.climb-status')
-    if (localStorage.getItem('climbStatus')) {
-    input.value = localStorage.getItem('climbStatus')
-    } else {
-        localStorage.setItem('climbStatus', input.value)
-    }
-}
